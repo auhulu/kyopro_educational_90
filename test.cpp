a34c0ceca852fe8dd6c72ce565386ae3b95faf57
auhulu <auhulu0@gmail.com>
2021-08-19 22:01:13 +0900

    first

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<long long, long long> P;
+#define rep(i, n) for (long long i = 0; i < (long long)(n); i++)
+#define agewari(a, b) ((long long)a + ((long long)b - 1)) / b
+const long long MOD = 1000000007;
+const long long INF = 1LL << 60;
+int main()
+{
+    rep(i,100)cout<<i<<endl;
+    cout<<agewari(3,2)<<endl;
+}
